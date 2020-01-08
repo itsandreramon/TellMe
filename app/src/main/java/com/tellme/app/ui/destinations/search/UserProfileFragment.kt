31868097328163053dee6aab99b2478c46b60389
andrethiele <thielea@th-brandenburg.de>
2020-01-08 16:06:44 +0100

    (App) : Update UserProfile followers count

--- app/src/main/java/com/tellme/app/ui/destinations/search/UserProfileFragment.kt
+++ app/src/main/java/com/tellme/app/ui/destinations/search/UserProfileFragment.kt
@@ -108,7 +108,7 @@ class UserProfileFragment : Fragment() {
     private fun renderUserData() {
         binding.user = args.user
         binding.imageViewUserAvatar.setUserProfileImageFromPath(args.user.avatar)
-        binding.textViewUserFollowerCount.text = getString(R.string.follower_count, 0)
+        binding.textViewUserFollowerCount.text = getString(R.string.follower_count, args.user.followers.size)
         binding.textViewUserFollowingCount.text = getString(R.string.following_count, args.user.following.size)
         binding.textViewUserTellCount.text = getString(R.string.tells_count, 0)
         binding.editTextSendUserTell.hint = getString(R.string.send_user_tell, args.user.name)
