d85f755d83b572360e221eb70b36c2df948710b5
andrethiele <thielea@th-brandenburg.de>
2020-01-24 14:08:22 +0100

    (App) : Update copyright

--- app/src/main/java/com/tellme/app/data/database/TellRoomDao.kt
+++ app/src/main/java/com/tellme/app/data/database/TellRoomDao.kt
@@ -1,8 +1,8 @@
 /*
- * Copyright 2020 - André Thiele
+ * Copyright 2020 - André Ramon Thiele
  *
- * Fachbereich Informatik und Medien
- * Technische Hochschule Brandenburg
+ * Department of Computer Science and Media
+ * University of Applied Sciences Brandenburg
  */
 
 package com.tellme.app.data.database
