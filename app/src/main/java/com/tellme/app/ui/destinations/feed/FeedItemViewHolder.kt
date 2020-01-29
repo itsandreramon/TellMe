f866703ca92b5d910e641a7537d2ee3c4eac2d73
andrethiele <thielea@th-brandenburg.de>
2020-01-29 13:19:06 +0100

    (App) : Fix crash when invalid date exists

--- app/src/main/java/com/tellme/app/ui/destinations/feed/FeedItemViewHolder.kt
+++ app/src/main/java/com/tellme/app/ui/destinations/feed/FeedItemViewHolder.kt
@@ -12,13 +12,20 @@ import com.tellme.app.extensions.setUserProfileImageFromPath
 import com.tellme.app.model.FeedItem
 import com.tellme.app.util.DateUtils
 import com.tellme.databinding.ViewHolderItemFeedBinding
+import java.time.format.DateTimeParseException
 
 class FeedItemViewHolder(val binding: ViewHolderItemFeedBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(feedItem: FeedItem, listener: FeedItemViewAdapter.FeedClickListener) {
         binding.feedItem = feedItem
         binding.root.setOnClickListener { listener.onFeedClicked(feedItem) }
-        binding.textViewDate.text = DateUtils.convertDate(feedItem.replyDate)
+
+        binding.textViewDate.text = try {
+            DateUtils.convertDate(feedItem.replyDate)
+        } catch (e: org.threeten.bp.format.DateTimeParseException) {
+            DateUtils.convertDate(DateUtils.now())
+        }
+
         binding.textViewUserUsername.text = feedItem.receiverUsername
         binding.imageViewUserAvatar.setUserProfileImageFromPath(feedItem.receiverPhotoUrl)
         binding.executePendingBindings()
